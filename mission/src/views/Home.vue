b39084ab7b76ee29aa37950ffb67ef3c5e1ead88
Sangwoo <psu8430@gmail.com>
2022-01-07 01:10:05 +0900

    Solve 1 week Mission

--- mission/src/views/Home.vue
+++ mission/src/views/Home.vue
@@ -2,6 +2,13 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div class="rotate">
+      <input v-bind:value="word" v-on:input="changeWord"/>
+      <button v-on:click="rotateWord">Run</button>
+    </div>
+    <p>{{word}}</p>
+    <p v-if="word != ''">Rotate Times : {{count}}</p>
   </div>
 </template>
 
@@ -14,5 +21,30 @@ export default {
   components: {
     HelloWorld,
   },
+  data() {
+    return {
+      word: '',
+      count: 0,
+    };
+  },
+  methods: {
+    changeWord(e) {
+      this.word = e.target.value;
+      this.count = this.word === '' ? 0 : this.count;
+    },
+    rotateWord() {
+      this.word = this.word[this.word.length - 1] + this.word.slice(0, this.word.length - 1);
+      this.count += 1;
+    },
+  },
 };
 </script>
+
+<style>
+.rotate input {
+  margin-right : 10px;
+}
+.rotate button {
+  padding : 3px, 5px;
+}
+</style>
