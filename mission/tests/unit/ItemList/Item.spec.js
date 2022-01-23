0d077c27b8edb397ff618690cb589bf997261dca
Sangwoo Park <psu8430@gmail.com>
2022-01-23 13:41:28 +0900

    Test: Add New test line about item & item list

--- mission/tests/unit/ItemList/Item.spec.js
+++ mission/tests/unit/ItemList/Item.spec.js
@@ -1,10 +1,140 @@
-import { mount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import ItemInfoPage from '@/components/ItemList/Item.vue';
 
-describe('ItemListItem', () => {
-  it('redners ItemListItem', () => {
-    const wrapper = mount(ItemInfoPage);
+describe('Item', () => {
+  const testUrl = 'www.google.com';
+  const testTitle = 'TestTitle';
+  const testCost = 30000;
+  const testDiscountTrue = { isDiscount: true, rate: 10 };
+  const testDiscountFalse = { isDiscount: false, rate: 0 };
+  const testUserRate = [4.8, 4.3, 4.7];
+  const testDiscountedCost = 27000;
+  const testReviewLength = 3;
+  const testAverageRate = 4.6;
 
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallowMount(ItemInfoPage, {
+      propsData: {
+        item: {
+          id: 1102323,
+          imgUrl: testUrl,
+          title: testTitle,
+          cost: testCost,
+          discount: testDiscountTrue,
+          userRate: testUserRate,
+          discountedCost: testDiscountedCost,
+          reviewLength: testReviewLength,
+          averageRate: testAverageRate,
+        },
+      },
+    });
+  });
+
+  it('renders ItemListItem', () => {
     expect(wrapper.find('.item-list-item').exists()).toBe(true);
+  });
+
+  describe('Product Image', () => {
+    it('contains prodcut image', async () => {
+      expect(wrapper.find('[data-test="item-image"]').exists()).toBeTruthy();
+    });
+
+    it('renders product image', async () => {
+      expect(wrapper.find('[data-test="item-image"]').attributes().src).toEqual(testUrl);
+    });
+
+    it('renders "No image"', async () => {
+      const basicUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-nOAglpmejsvQmil3kr19lwURHplsMvhv5A&usqp=CAU';
+      await wrapper.setProps({
+        item: {
+          id: 1102323,
+          imgUrl: '',
+          title: testTitle,
+          cost: testCost,
+          discount: testDiscountTrue,
+          userRate: testUserRate,
+          discountedCost: testDiscountedCost,
+          reviewLength: testReviewLength,
+          averageRate: testAverageRate,
+        },
+      });
+      expect(wrapper.find('[data-test="item-image"]').attributes().src).toEqual(basicUrl);
+    });
+  });
+
+  describe('Product Title', () => {
+    it('contains product title', async () => {
+      expect(wrapper.find('[data-test="item-title"]').exists()).toBeTruthy();
+    });
+
+    it('renders product title', async () => {
+      expect(wrapper.find('[data-test="item-title"]').text()).toEqual(testTitle);
+    });
+  });
+
+  describe('Product Rate', () => {
+    it('contains product rate', async () => {
+      expect(wrapper.find('[data-test="item-averagerate"]').exists()).toBeTruthy();
+    });
+
+    it('renders product average rate', async () => {
+      expect(wrapper.find('[data-test="item-averagerate"]').text()).toContain('4.6');
+    });
+
+    it('renders the number of reviews', async () => {
+      expect(wrapper.find('[data-test="item-averagerate"]').text()).toContain('(3)');
+    });
+
+    it('renders average rate when it has not product review', async () => {
+      await wrapper.setProps({
+        item: {
+          id: 1102323,
+          imgUrl: testUrl,
+          title: testTitle,
+          cost: testCost,
+          discount: testDiscountTrue,
+          userRate: [],
+          discountedCost: testDiscountedCost,
+          reviewLength: 0,
+          averageRate: '0.00',
+        },
+      });
+
+      expect(wrapper.find('[data-test="item-averagerate"]').text()).toContain('0.00');
+      expect(wrapper.find('[data-test="item-averagerate"]').text()).toContain('(0)');
+    });
+  });
+
+  it('contains product discount info if isDiscount is True', async () => {
+    expect(wrapper.find('[data-test="item-discount-rate"]').exists()).toBeTruthy();
+    expect(wrapper.find('[data-test="item-origin-cost"]').exists()).toBeTruthy();
+    expect(wrapper.find('[data-test="item-discounted-cost"]').exists()).toBeTruthy();
+  });
+
+  it('renders product discount info if isDiscount is True', async () => {
+    expect(wrapper.find('[data-test="item-origin-cost"]').text()).toEqual('30,000원');
+    expect(wrapper.find('[data-test="item-discount-rate"]').text()).toEqual('10%');
+    expect(wrapper.find('[data-test="item-discounted-cost"]').text()).toEqual('27,000원');
+  });
+
+  it('contains product discount info if isDiscount is False', async () => {
+    await wrapper.setProps({
+      item: {
+        id: 1102323,
+        imgUrl: testUrl,
+        title: testTitle,
+        cost: testCost,
+        discount: testDiscountFalse,
+        userRate: testUserRate,
+        discountedCost: testDiscountedCost,
+        reviewLength: testReviewLength,
+        averageRate: testAverageRate,
+      },
+    });
+
+    expect(wrapper.find('[data-test="item-discount-rate"]').exists()).toBeFalsy();
+    expect(wrapper.find('[data-test="item-origin-cost"]').exists()).toBeFalsy();
+    expect(wrapper.find('[data-test="item-discounted-cost"]').exists()).toBeTruthy();
   });
 });
--- mission/tests/unit/ItemList/ItemList.spec.js
+++ mission/tests/unit/ItemList/ItemList.spec.js
@@ -1,10 +1,71 @@
 import { mount } from '@vue/test-utils';
 import ItemInfoPage from '@/views/ItemList.vue';
 
-describe('ItemListPage', () => {
-  it('redners ItemListPage', () => {
-    const wrapper = mount(ItemInfoPage);
+let wrapper;
 
-    expect(wrapper.find('#item-list-page').exists()).toBe(true);
+describe('ItemListPage', () => {
+  beforeEach(() => {
+    wrapper = mount(ItemInfoPage);
+  });
+
+  it('renders ItemListPage', () => {
+    expect(wrapper.find('#item-list-page').exists()).toBeTruthy();
+  });
+
+  describe('Header', () => {
+    it('contains header', () => {
+      expect(wrapper.find('#header').exists()).toBeTruthy();
+    });
+
+    it('renders header titles', () => {
+      expect(wrapper.find('[data-test="header"]').text()).toEqual('🦁 LionAbly');
+    });
+  });
+
+  describe('Item Sort', () => {
+    it('contiains sort points', () => {
+      expect(wrapper.find('[data-test="sort-btn"]').exists()).toBeTruthy();
+    });
+
+    it('renders several buttons', async () => {
+      await wrapper.setData({
+        sortPoint: ['기준 1', '기준 2', '기준 3'],
+      });
+
+      expect(wrapper.findAll('.sort-btn').length).toBe(4);
+    });
+
+    it('renders button name with each points', async () => {
+      await wrapper.setData({
+        sortPoint: ['기준 1', '기준 2', '기준 3'],
+      });
+
+      expect(wrapper.find('[data-test="sort-btn"]').text()).toBe('정렬  # 기준 1 # 기준 2 # 기준 3');
+    });
+  });
+
+  describe('Item List', () => {
+    it('contains item list', () => {
+      expect(wrapper.find('[data-test="item-list"]').exists()).toBeTruthy();
+    });
+  });
+
+  describe('Navigation Bar', () => {
+    it('renders navbar', () => {
+      expect(wrapper.find('#navigation').exists()).toBeTruthy();
+    });
+
+    it('contains 4 buttons', () => {
+      expect(wrapper.findAll('.navigation-btn').length).toBe(4);
+    });
+
+    it('renders 4 buttons', () => {
+      const buttonContents = ['Home', 'Like', 'Cart', 'My Page'];
+      const buttons = wrapper.findAll('.navigation-button');
+
+      buttons.forEach((b, i) => {
+        expect(b.text()).toEqual(buttonContents[i]);
+      });
+    });
   });
 });
