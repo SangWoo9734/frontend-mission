562bbee208e092d81f442eec1a1711c1d1ad1753
Sangwoo Park <psu8430@gmail.com>
2022-01-23 13:46:29 +0900

    Feat: Add Item List Page

--- mission/src/views/ItemList.vue
+++ mission/src/views/ItemList.vue
@@ -1,12 +1,186 @@
 <template>
-  <div id="item-list-page"></div>
+  <div id='item-list-page' class='container'>
+    <!-- Header -->
+    <TheHeader />
+    <!-- SortButton -->
+    <div class='sort-btn-container flex' data-test='sort-btn'>
+      <div class='sort-btn-viewbox flex'>
+        <div class='sort-btn' id='sort-fixed-btn'>
+        <font-awesome-icon :icon="['fas', 'filter']" />
+        정렬
+        </div>
+        <div class='sort-btn' v-for='point in sortPoint' :key='point' @click='sortItem(point)'>
+          # {{ point }}
+        </div>
+      </div>
+    </div>
+    <!-- ItemList -->
+    <div class='item-list' data-test='item-list'>
+      <Item v-for='item in itemData' :key='item.id' :item='item' />
+    </div>
+    <!-- Navbar -->
+    <TheNavbar />
+  </div>
 </template>
 
 <script>
+import TheHeader from '../components/ItemCommon/TheHeader.vue';
+import Item from '../components/ItemList/Item.vue';
+import TheNavbar from '../components/ItemList/TheNavbar.vue';
+
 export default {
   name: 'ItemListPage',
+  data() {
+    return {
+      itemData: [
+        {
+          id: 1102321,
+          imgUrl: 'https://image.msscdn.net/images/goods_img/20190923/1163605/1163605_2_500.jpg',
+          title: 'NIKE F/W Sweater',
+          cost: 30000,
+          discount: { isDiscount: true, rate: 15 },
+          userRate: [4.8, 4.3, 4.1, 4.5, 4.7],
+          uploadData: '2022-01-01',
+          like: false,
+        },
+        {
+          id: 1102323,
+          imgUrl: 'https://www.socks-house.com/shopimages/soxhousem/0340060000183.jpg',
+          title: 'School 스포츠 긴 양말 (Green)',
+          cost: 12000,
+          discount: { isDiscount: true, rate: 10 },
+          userRate: [4.4, 4.2, 3.7],
+          uploadData: '2022-01-02',
+          like: false,
+        },
+        {
+          id: 1102325,
+          imgUrl: 'https://media.catchfashion.com/images/h_l?id=4ZmBUsDVn&t=ps',
+          title: 'B-Street Athlete Runner',
+          cost: 89000,
+          discount: { isDiscount: false, rate: 0 },
+          userRate: [4.6, 4.5],
+          uploadData: '2022-01-15',
+          like: false,
+        },
+        {
+          id: 1102327,
+          imgUrl: '',
+          title: '무지 기모 후드 (BEIGE)',
+          cost: 42000,
+          discount: { isDiscount: true, rate: 20 },
+          userRate: [4.5, 4.2, 4.0, 3.9],
+          uploadData: '2022-01-11',
+          like: false,
+        },
+        {
+          id: 1102329,
+          imgUrl: 'https://static.mlb-korea.com/images/goods/detail/m/20201123/32CP77111-50L-22150463920731175.jpg/dims/resize/828x828',
+          title: 'MLB 뉴욕양키스 (BLACK)',
+          cost: 39000,
+          discount: { isDiscount: true, rate: 10 },
+          userRate: [],
+          uploadData: '2022-01-21',
+          like: false,
+        },
+      ],
+      sortPoint: ['최신순', '높은 할인율', '높은 평점순', '낮은 가격순', '높은 가격순', '리뷰 많은순'],
+    };
+  },
+  components: {
+    TheHeader,
+    Item,
+    TheNavbar,
+  },
+  methods: {
+    sortAscending(point) {
+      this.itemData.sort((a, b) => {
+        if (a[point] > b[point]) return 1;
+        if (a[point] === b[point]) return 0;
+        if (a[point] < b[point]) return -1;
+        return 0;
+      });
+    },
+    sortDesending(point) {
+      this.itemData.sort((a, b) => {
+        if (a[point] < b[point]) return 1;
+        if (a[point] === b[point]) return 0;
+        if (a[point] > b[point]) return -1;
+        return 0;
+      });
+    },
+
+    sortItem(point) {
+      if (point === '최신순') this.sortDesending('id');
+      if (point === '높은 할인율') {
+        this.itemData.sort((a, b) => {
+          if (a.discount.rate < b.discount.rate) return 1;
+          if (a.discount.rate === b.discount.rate) return 0;
+          if (a.discount.rate > b.discount.rate) return -1;
+          return 0;
+        });
+      }
+      if (point === '높은 평점순') this.sortDesending('averageRate');
+      if (point === '낮은 가격순') this.sortAscending('discountedCost');
+      if (point === '높은 가격순') this.sortDesending('discountedCost');
+      if (point === '리뷰 많은순') this.sortDesending('reviewLength');
+    },
+  },
+  created() {
+    this.itemData.forEach((data) => {
+      const userReviewLength = data.userRate.length;
+      let rateSum = 0;
+      data.userRate.forEach((r) => {
+        rateSum += r;
+      });
+
+      this.data = Object.assign(data, {
+        discountedCost: data.cost * (1 - data.discount.rate / 100),
+        reviewLength: userReviewLength,
+        averageRate: userReviewLength !== 0 ? (rateSum / userReviewLength).toFixed(2) : '0.00',
+        isNew: (Date.now() - new Date(data.uploadData).getTime()) / (1000 * 3600 * 24) < 7,
+      });
+    });
+  },
 };
 </script>
 
-<style>
+<style scoped>
+.item-list {
+  padding : 10px 7px 100px 7px;
+}
+.item-list:after {
+    content: '';
+    display: block;
+    clear: both;
+}
+.sort-btn-container{
+  width: 100%;
+  height: 50px;
+  margin-top: 20px;
+  overflow: auto;
+  align-items: center;
+}
+.sort-btn-viewbox {
+  width: fit-content;
+  align-items: center;
+  padding: 0 10px;
+}
+#sort-fixed-btn{
+  border: 1px solid gray;
+  background: white;
+  width: 60px;
+}
+.sort-btn {
+  padding: 5px 10px;
+  margin-right: 10px;
+  width: max-content;
+  float: left;
+  border-radius: 25px;
+  border: 2px solid rgb(255, 209, 123);
+}
+.sort-btn:hover{
+  cursor: pointer;
+}
+
 </style>
