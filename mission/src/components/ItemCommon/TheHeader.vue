c18c688c2eb756b44e919ab2367e462254a8aee2
Sangwoo Park <psu8430@gmail.com>
2022-01-26 15:22:12 +0900

    Feat: Add Header Action

--- mission/src/components/ItemCommon/TheHeader.vue
+++ mission/src/components/ItemCommon/TheHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id='header' class='flex' data-test='header'>
+    <div id='header' :class='showHeader ? "flex" :  "header-none flex" ' data-test='header'>
       <h2>🦁 LionAbly</h2>
     </div>
 </template>
@@ -7,11 +7,45 @@
 <script>
 export default {
   name: 'TheNavbar',
+  data() {
+    return {
+      showHeader: true,
+    };
+  },
+  methods: {
+    scroll() {
+      let debouncer;
+      const currentScrollPosition = window.pageYOffset;
+
+      if (currentScrollPosition < 0) {
+        return;
+      }
+
+      if (debouncer) {
+        clearTimeout(debouncer);
+      }
+
+      debouncer = setTimeout(() => {
+        if (currentScrollPosition < 60) this.showHeader = true;
+        else this.showHeader = false;
+      }, 100);
+    },
+  },
+  mounted() {
+    // window.scrollTo(0, 0);
+    document.addEventListener('scroll', this.scroll);
+  },
+
+  unmounted() {
+    document.removeEventListener('scroll', this.scroll);
+  },
 };
 </script>
 
 <style scoped>
 #header {
+  position: fixed;
+  top: 0;
   max-width:500px;
   width: 100%;
   height: 60px;
@@ -20,10 +54,16 @@ export default {
   background: white;
   align-items: center;
   z-index: 10;
+  transition: 0.4s all ease-out;
 }
 #header h2{
   width: 100%;
   text-align: left;
   margin-left : 15px;
+}
+
+.header-none {
+  box-shadow: none;
+  transform: translate(0, -100%);
 }
 </style>
