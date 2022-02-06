618be84321319c543412abb8e9f7662e54cc851a
Sangwoo Park <psu8430@gmail.com>
2022-02-06 19:13:12 +0900

    Chore: Delete Axios call lines

--- mission/src/main.js
+++ mission/src/main.js
@@ -6,13 +6,27 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 
+// import axios from 'axios';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 library.add(fas, far);
 
-createApp(App)
+// axios.defaults.baseURL = '';
+
+// axios.defaults.headers.common['Content-Type'] = 'application/json';
+
+// axios.defaults.headers.common.Authorization = 'abcd1234';
+
+// axios.defaults.headers.common['Access-Control-Allow-Origin'] = 'https://virtserver.swaggerhub.com/lkaybob/projectlion-vue/1.0.0/item';
+
+const app = createApp(App);
+
+// app.config.globalProperties.axios = axios;
+
+app
   .component('font-awesome-icon', FontAwesomeIcon)
   .use(store)
   .use(router)
