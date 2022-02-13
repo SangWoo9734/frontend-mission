15611bd7be694dea52a5f48cb7d7b7a1ed8d8af4
Sangwoo Park <psu8430@gmail.com>
2022-02-13 21:50:15 +0900

    Chore: Add Code of animation.css

--- mission/src/App.vue
+++ mission/src/App.vue
@@ -18,6 +18,8 @@ export default {
 </script>
 <style>
 @import url('./assets/style/common.css');
+@import url('./assets/style/animation.css');
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
